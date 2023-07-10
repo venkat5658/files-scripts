0c34775f8ca31872c5f1d74901f78ab9f1b52ffc
venkat5658 <113323607+venkat5658@users.noreply.github.com>
2023-07-10 10:53:34 +0530

    Update jenkinsInstal.sh

--- jenkinsInstal.sh
+++ jenkinsInstal.sh
@@ -12,6 +12,12 @@ sudo apt-get install jenkins # to install jenkins
 sudo systemctl start jenkins.service #to start the services of jenkins
 sudo systemctl status jenkins #to check the status of the jenkins
 
+ # curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+# echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
 # after instalation to get the passwor from sudo cat  /var/lib/jenkins/secrets/initialAdminPassword
 #install the plugins and ater give the proper username,password your fullname,your emails id.
 #after Then specify the preferred Jenkins URL and finish the configuration process
